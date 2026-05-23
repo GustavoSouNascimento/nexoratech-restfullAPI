1150493e47515265b5eabc5d8241150751a503ac
GustavoSouNascimento <gustavosouza.nasci@gmail.com>
2026-05-23 20:13:02 -0300

    Correção de erro em CorsFilter

--- src/main/java/br/com/nexora/CorsFilter.java
+++ src/main/java/br/com/nexora/CorsFilter.java
@@ -1,18 +1,24 @@
 package br.com.nexora;
 
 import jakarta.ws.rs.container.*;
+import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.Provider;
 import java.io.IOException;
 
 @Provider
-public class CorsFilter implements ContainerResponseFilter {
+public class CorsFilter implements ContainerRequestFilter {
 
     @Override
-    public void filter(ContainerRequestContext requestContext,
-                       ContainerResponseContext responseContext) throws IOException {
-
-        responseContext.getHeaders().putSingle("Access-Control-Allow-Origin", "*");
-        responseContext.getHeaders().putSingle("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
-        responseContext.getHeaders().putSingle("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    public void filter(ContainerRequestContext requestContext) throws IOException {
+        if ("OPTIONS".equalsIgnoreCase(requestContext.getMethod())) {
+            requestContext.abortWith(
+                    Response.ok()
+                    .header("Access-Control-Allow-Origin", "*")
+                    .header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
+                    .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+                    .header("Access-Control-Max-Age", "86400")
+                    .build()
+            );
+        }
     }
 }
