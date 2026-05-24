4e3565a4153b7bb9ffcec147b1e8e792c1919a61
GustavoSouNascimento <gustavosouza.nasci@gmail.com>
2026-05-24 17:24:06 -0300

    Adição de ContainerResponseContext ao CorsFilter

--- src/main/java/br/com/nexora/CorsFilter.java
+++ src/main/java/br/com/nexora/CorsFilter.java
@@ -6,19 +6,30 @@ import jakarta.ws.rs.ext.Provider;
 import java.io.IOException;
 
 @Provider
-public class CorsFilter implements ContainerRequestFilter {
+@PreMatching
+public class CorsFilter implements ContainerRequestFilter, ContainerResponseFilter {
 
     @Override
     public void filter(ContainerRequestContext requestContext) throws IOException {
         if ("OPTIONS".equalsIgnoreCase(requestContext.getMethod())) {
             requestContext.abortWith(
                     Response.ok()
-                    .header("Access-Control-Allow-Origin", "*")
-                    .header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
-                    .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-                    .header("Access-Control-Max-Age", "86400")
-                    .build()
+                            .header("Access-Control-Allow-Origin", "*")
+                            .header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
+                            .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
+                            .header("Access-Control-Max-Age", "86400")
+                            .build()
             );
         }
+    }
+
+    @Override
+    public void filter(ContainerRequestContext requestContext,
+                       ContainerResponseContext responseContext) throws IOException {
+        responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
+        responseContext.getHeaders().add("Access-Control-Allow-Headers",
+                "origin, content-type, accept, authorization");
+        responseContext.getHeaders().add("Access-Control-Allow-Methods",
+                "GET, POST, PUT, DELETE, OPTIONS, HEAD");
     }
 }
